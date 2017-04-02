b5df8c915a45fc594ced88623199697d0c43588a
Song Bo <songbo@songs-macbook-pro.local>
2017-04-02 17:16:42 +0800

    c

--- AlgorithmExercise/RBViewController.swift
+++ AlgorithmExercise/RBViewController.swift
@@ -12,20 +12,26 @@ class RBViewController: UIViewController {
 
     @IBOutlet weak var tf: UITextField!
     let t = RBTree<Int>()
+    let scrollView = UIScrollView()
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.barTintColor = UIColor.white
         navigationController?.navigationBar.isTranslucent = false
+        scrollView.frame = view.bounds
+        scrollView.contentSize = view.bounds.size
+        view.addSubview(scrollView)
         
 //        t.insert(e: 105)
 //        t.insert(e: 100)
 //        t.insert(e: 99)
-        stride(from: 500, through: 300, by: -10).forEach { (i) in
-            t.insert(e: i)
-        }
-        [445,435,437,433].forEach { (i) in
-            t.insert(e: i)
-        }
+//        stride(from: 500, through: 100, by: -10).forEach { (i) in
+//            t.insert(e: i)
+//        }
+//        [445,435,437,433].forEach { (i) in
+//            t.insert(e: i)
+//        }
+        (1...100).forEach{_ in
+            t.insert(e: Int(arc4random_uniform(500)))}
         drawRBTree(t: t)
     }
     @IBAction func add(_ sender: UIBarButtonItem) {
@@ -42,9 +48,17 @@ class RBViewController: UIViewController {
     }
     
     func drawRBTree<T:Comparable>(t:RBTree<T>){
-        for v in view.subviews{
+        for v in scrollView.subviews{
             v.removeFromSuperview()
         }
+        
+        if let layers = scrollView.layer.sublayers{
+            for l in layers {
+                l.removeFromSuperlayer()
+            }
+        }
+        
+
         
         let nodeW:CGFloat = 25
         let rowH = nodeW
@@ -52,8 +66,14 @@ class RBViewController: UIViewController {
         func addLine(p1:CGPoint,p2:CGPoint){
             let line = CAShapeLayer()
             let linePath = UIBezierPath()
-            linePath.move(to: p1)
-            linePath.addLine(to: p2)
+            let r = nodeW / 2
+            let sin = (p2.y - p1.y) / (p2.distanceTo(p1))
+            let cos = (p2.x - p1.x) / (p2.distanceTo(p1))
+            let v = CGVector(dx: r * cos, dy: r * sin)
+            let a1 = p1 + v
+            let a2 = p2 - v
+            linePath.move(to: a1)
+            linePath.addLine(to: a2)
             line.lineWidth = 1.0
             line.path = linePath.cgPath
 //            let d = p1.distanceTo(p2)
@@ -61,10 +81,10 @@ class RBViewController: UIViewController {
 //            line.lineDashPattern = [NSNumber(value: Float(Float(nodeW / 2))),NSNumber(value: Float(Float(d - nodeW))),NSNumber(value: Float(Float(nodeW / 2)))]
 //            line.fillColor = UIColor.black.cgColor
             line.strokeColor = UIColor.blue.cgColor
-            view.layer.addSublayer(line)
+            scrollView.layer.addSublayer(line)
         }
 
-        func addRBNode(n:RBNode<T>,center:CGPoint){
+        func addRBNode(n:RBNode<T>,center:CGPoint,i:Int){
             let button = UIButton()
             button.frame.size = CGSize(width: nodeW, height: nodeW)
             button.isEnabled = false
@@ -74,13 +94,19 @@ class RBViewController: UIViewController {
             button.setTitle(String(describing: n.element), for: .normal)
             button.layer.cornerRadius = nodeW / 2
             button.center = center
-            view.addSubview(button)
+            scrollView.addSubview(button)
             
-//            if n.left != nil {
-//                let p1 = center
-//                let p2 = CGPoint(x: p1.x - nodeW / 2, y: p1.y + rowH)
-//                addLine(p1: p1, p2: p2)
-//            }
+            if n.left != nil {
+                let p1 = center
+                let p2 = CGPoint(x: p1.x - pow(2, CGFloat(i - 1)) * (nodeW / 2), y: p1.y + rowH)
+                addLine(p1: p1, p2: p2)
+            }
+            
+            if n.right != nil {
+                let p1 = center
+                let p2 = CGPoint(x: p1.x + pow(2, CGFloat(i - 1)) * (nodeW / 2), y: p1.y + rowH)
+                addLine(p1: p1, p2: p2)
+            }
         }
         
         guard let n = t.root else{
@@ -110,16 +136,22 @@ class RBViewController: UIViewController {
         let rowNumber = rowArr.count
         var curX:CGFloat
         var curY:CGFloat = rowH * CGFloat(rowNumber) - nodeW * 1 / 2
+        if (curY + rowH)  > scrollView.contentSize.height{
+            scrollView.contentSize.height = curY + rowH
+        }
         for (i,row) in rowArr.reversed().enumerated(){
             curX = i == 0 ? (nodeW * 1 / 2) : (nodeW * pow(2, CGFloat(i - 1 )))
             for n in row{
                 if n != nil {
-                    addRBNode(n: n!, center: CGPoint(x: curX, y: curY))
+                    addRBNode(n: n!, center: CGPoint(x: curX, y: curY),i: i)
                 }
                 let spaceX = nodeW * pow(2, CGFloat(i))
 //                print("spaceX: \(spaceX)")
                 curX += spaceX
 //                curX += nodeW
+            }
+            if curX > scrollView.contentSize.width{
+                scrollView.contentSize.width = curX
             }
             curY -= rowH
         }
@@ -134,16 +166,13 @@ class RBViewController: UIViewController {
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
 }
+
+class RBButton: UIButton {
+    var leftButton:RBButton?
+    var rightButton:RBButton?
+    var leftLine:CAShapeLayer?
+    var rightLine:CAShapeLayer?
+}
+
